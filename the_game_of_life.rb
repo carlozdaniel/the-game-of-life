392179c1976013822ecdc480c5011801628d4581
Alvaro <varoboss.ap@gmail.com>
2020-01-30 12:47:28 -0600

    Refactor game_of_life main

--- the_game_of_life.rb
+++ the_game_of_life.rb
@@ -1,43 +1,4 @@
-class The_game_of_life
-  #life = ▇
-  #death = ▢
- 
-  board_size = gets.chomp.to_i
-  Row = board_size
-  Column = board_size
-  @VIDA = " ▇"
-  @MUERTA = " ▢"
+require_relative 'board'
+require_relative 'cell'
 
-  def print_board() #print 
-    
-    @board = empty_board
-    @board.each do |fila|
-      fila.each do |celula|
-        if rand(10) == 1
-          @VIDA 
-        else
-          @MUERTA 
-        end
-      end
-    end
-  end
-
-  def empty_board
-    Array.new(Row) do 
-      Array.new(Column) do
-      end 
-    end 
-  end
-
-  def print_space_in_terminal
-    puts "\n"
- end
- 
-
-
-end
-
-obj = The_game_of_life.new()
-obj.print_board
-obj.empty_board
-obj.print_space_in_terminal
+new_board = Board.new(gets.chomp.to_i)

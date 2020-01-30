e01124e1ecde1a16b8e37d0892744c21a6dcfa34
Alvaro <varoboss.ap@gmail.com>
2020-01-30 14:00:06 -0600

    Determine cell item

--- cell.rb
+++ cell.rb
@@ -1,25 +1,32 @@
 class Cell
-    def initialize 
-      @alive=false
-    end
+  ITEMS = { alive: " ▇", dead: " ▢"}
 
-    def alive!
-      @alive = true
-    end
+  attr_reader :item
 
-    def alive?
-      @alive
-    end
+  def initialize 
+    @alive = false
+    @item = ITEMS[:dead]
+  end
 
-    def kill!
-      @alive = false
-    end
+  def alive!
+    @alive = true
+    @item = ITEMS[:alive]
+  end
 
-    def stay_alive?(neighbors)
-      alive_neighbors = neighbors.filter(&:alive?).count 
-      return true if alive? && alive_neighbors >= 2 || alive_neighbors <= 3
-      return false if alive? && alive_neighbors <= 1 || alive_neighbors >= 4
-      return true if !alive? && alive_neighbors == 3
-      return false if !alive? && alive_neighbors != 3
-    end
+  def alive?
+    @alive
+  end
+
+  def kill!
+    @alive = false
+    @item = ITEMS[:dead]
+  end
+
+  def stay_alive?(neighbors)
+    alive_neighbors = neighbors.filter(&:alive?).count 
+    return true if alive? && alive_neighbors >= 2 || alive_neighbors <= 3
+    return false if alive? && alive_neighbors <= 1 || alive_neighbors >= 4
+    return true if !alive? && alive_neighbors == 3
+    return false if !alive? && alive_neighbors != 3
+  end
 end

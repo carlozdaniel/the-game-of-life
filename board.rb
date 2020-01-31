3f5f2a69d894770c06ed2681540478542a7551c3
Carlos Daniel H.R <clear>
2020-01-31 13:30:37 -0600

    captura neighbords

--- board.rb
+++ board.rb
@@ -1,11 +1,12 @@
 class Board
-  attr_reader :panel
+  attr_reader :panel, :size
   def initialize(size)
     @size = size
     @panel = Array.new(size){Array.new(size) { Cell.new }}
   end
 
   def print_panel #print 
+    sleep 1
     @panel.each do |row|
       row.each { |cell| print cell.item }
       print "\n" 
@@ -17,5 +18,21 @@ class Board
       row.each { |cell| cell.alive! if rand(10) == 1 }
     end
   end
+
+  
+  def capture_neighbors(main_x, main_y)
+    neighbors = []
+    3.times do |row| 
+      neighbor_x = row - 1 + main_x  
+      3.times do |column|
+        neighbor_y = column - 1 + main_y
+        if validate_cell(neighbor_x,neighbor_y) 
+          neighbors << panel[neighbor_x][neighbor_y]
+        end
+      end 
+    end
+    neighbors 
+  end 
+
 
 end

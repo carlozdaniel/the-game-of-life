b2a66fe9cb3fb1eea45453bb5258f5bebecc9d52
Carlos Daniel H.R <clear>
2020-01-31 13:38:38 -0600

    call from method

--- cell.rb
+++ cell.rb
@@ -1,11 +1,19 @@
 class Cell
   ITEMS = { alive: " ▇", dead: " ▢"}
+  
 
   attr_reader :item
+  attr_accessor :next_state
 
   def initialize 
     @alive = false
     @item = ITEMS[:dead]
+    @next_state = false 
+  
+  end
+
+  def capture_next_state
+   next_state ? alive! : kill! 
   end
 
   def alive!
@@ -23,10 +31,14 @@ class Cell
   end
 
   def stay_alive?(neighbors)
-    alive_neighbors = neighbors.filter(&:alive?).count 
+    alive_neighbors = neighbors.select{|element| element.alive?}.count   #(&:alive?).count 
     return true if alive? && alive_neighbors >= 2 || alive_neighbors <= 3
     return false if alive? && alive_neighbors <= 1 || alive_neighbors >= 4
     return true if !alive? && alive_neighbors == 3
     return false if !alive? && alive_neighbors != 3
   end
+
+  
+
+
 end
--- the_game_of_life.rb
+++ the_game_of_life.rb
@@ -1,4 +1,10 @@
 require_relative 'board'
 require_relative 'cell'
-
+puts "dame el tamaño del tablero"
 new_board = Board.new(gets.chomp.to_i)
+new_board.random_fill
+loop do
+  new_board.print_panel
+  new_board.next_iteration
+end
+ 

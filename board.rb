b2766c0583a3daefaf738ac27b49dacc6b475d83
Carlos Daniel H.R <clear>
2020-02-04 09:28:37 -0600

    code debugging board

--- board.rb
+++ board.rb
@@ -1,15 +1,17 @@
+# good
+# Description/Explanation of Person class
 class Board
   attr_reader :panel, :size
   def initialize(size)
     @size = size
-    @panel = Array.new(size){Array.new(size) { Cell.new }}
+    @panel = Array.new(size) { Array.new(size) { Cell.new } }
   end
 
-  def print_panel #print 
+  def print_panel
     puts `clear`
     @panel.each do |row|
       row.each { |cell| print cell.item }
-      print "\n" 
+      print "\n"
     end
     sleep 1
   end
@@ -22,22 +24,22 @@ class Board
 
   def capture_neighbors(main_x, main_y)
     neighbors = []
-    3.times do |row| 
-      neighbor_x = row - 1 + main_x  
+    3.times do |row|
+      neighbor_x = row - 1 + main_x
       3.times do |column|
         neighbor_y = column - 1 + main_y
-        if validate_cell([main_x, main_y], [neighbor_x,neighbor_y]) 
+        if validate_cell([main_x, main_y], [neighbor_x, neighbor_y])
           neighbors << panel[neighbor_x][neighbor_y]
         end
-      end 
+      end
     end
     neighbors
-  end 
+  end
 
   def validate_cell(main, index)
-    if !index[0].negative? && index[0] < size && !index[1].negative? && index[1] < size && index != main 
-      true 
-    else 
+    if !index[0].negative? && index[0] < size && !index[1].negative? && index[1] < size && index != main
+      true
+    else
       false
     end
   end
@@ -45,17 +47,17 @@ class Board
   def next_iteration
     panel.each_with_index do |row, x_position|
       row.each_with_index do |cell, y_position|
-        neighbors = capture_neighbors(x_position,y_position)
+        neighbors = capture_neighbors(x_position, y_position)
         cell.next_state = cell.stay_alive?(neighbors)
       end
-    end 
+    end
     new_state
   end
-  
+
   def new_state
     panel.each do |row|
       row.each do |cell|
-        cell.capture_next_state 
+        cell.capture_next_state
       end
     end
   end

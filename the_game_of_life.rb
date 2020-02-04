210d847d6976dec1f7c29a338227531492b9750a
Carlos Daniel H.R <clear>
2020-02-04 09:32:33 -0600

    code debugging main

--- the_game_of_life.rb
+++ the_game_of_life.rb
@@ -1,10 +1,9 @@
 require_relative 'board'
 require_relative 'cell'
-puts "dame el tamaño del tablero"
+puts 'dame el tamaño del tablero'
 new_board = Board.new(gets.chomp.to_i)
 new_board.random_fill
 loop do
   new_board.print_panel
   new_board.next_iteration
 end
- 
